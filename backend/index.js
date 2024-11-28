9db4f1b68853cacce4948a4610452c842b353ea7
RRicky25 <rickyratnani25@gmail.com>
2024-11-28 11:46:01 +0530

    update cors for k8 access

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,19 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
-    credentials: true
+  origin: [
+    'http://localhost:5173',
+    'https://book-app-frontend-tau.vercel.app',
+    'http://192.168.49.2:32559'
+
+  ],
+  credentials: true
 }))
 
 // routes
 const bookRoutes = require('./src/books/book.route');
 const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
+const userRoutes = require("./src/users/user.route")
 const adminRoutes = require("./src/stats/admin.stats")
 
 app.use("/api/books", bookRoutes)
